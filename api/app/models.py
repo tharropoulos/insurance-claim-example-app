80ea0b16329c19a990b00c8cfb773f4e238a2ad0
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-03-23 10:56:20 +0200

    feat(db): add images and claims table

--- api/app/models.py
+++ api/app/models.py
@@ -1,5 +1,6 @@
 from app import db
 from typing import Optional
+from datetime import datetime
 import sqlalchemy.orm as so
 import sqlalchemy as sa
 from flask_jwt_extended import get_jwt_identity
@@ -26,3 +27,46 @@ def load_user():
     email = get_jwt_identity()
     user = User.query.filter_by(email=email).first()
     return user
+
+class Claim(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    date_of_accident: so.Mapped[datetime] = so.mapped_column(sa.DateTime)
+    accident_type: so.Mapped[str] = so.mapped_column(sa.String(64))
+    description: so.Mapped[str] = so.mapped_column(sa.String(256))
+    injuries_reported: so.Mapped[bool] = so.mapped_column(sa.Boolean)
+    damage_details: so.Mapped[str] = so.mapped_column(sa.String(256))
+
+    author: so.Mapped[User] = so.relationship(back_populates="claims")
+    images: so.WriteOnlyMapped["Image"] = so.relationship(back_populates="claim")
+
+    def __repr__(self):
+        return "<Claim {}>".format(self.id)
+
+    def to_dict(self):
+        session = so.Session.object_session(self)
+        stmt = sa.select(Image).where(Image.claim_id == self.id)
+        images = session.execute(stmt).scalars().all()
+
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "date_of_accident": self.date_of_accident,
+            "accident_type": self.accident_type,
+            "description": self.description,
+            "injuries_reported": self.injuries_reported,
+            "damage_details": self.damage_details,
+            "author": self.author.id if self.author else None,
+            "images": [image.id for image in images],
+        }
+
+
+class Image(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    claim_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Claim.id), index=True)
+    image_file: so.Mapped[str] = so.mapped_column(sa.String(256))
+
+    claim: so.Mapped[Claim] = so.relationship(back_populates="images")
+
+    def __repr__(self):
+        return "<Image {}>".format(self.id)
