20fc3500312869d074ed06008edf323f05433d73
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-03-24 11:35:47 +0200

    feat(db): add turso connection

--- api/config.py
+++ api/config.py
@@ -8,8 +8,12 @@ load_dotenv(os.path.join(basedir, ".env"))
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
+        "SQLALCHEMY_DATABASE_URI"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {"check_same_thread": False, "timeout": 30},
+        "echo": True,
+    }
     UPLOAD_FOLDER = os.path.join(basedir, "uploads")
 
 
