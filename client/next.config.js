1522a05afbfd6e4f2e6472096c136a3bfc9481df
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-03-24 23:41:57 +0200

    fix: redirect requests to api url

--- client/next.config.js
+++ client/next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    rewrites: async () => {
+    redirects: async () => {
         return [
             {
                 source: "/api/:path*",
                 destination:
                     process.env.NODE_ENV === "development"
                         ? "http://127.0.0.1:5328/api/:path*"
-                        : "https://claims-api.fly.dev/api/auth/:path*",
+                        : "https://claims-api.fly.dev/api/:path*",
+                permanent: true,
             },
         ];
     },
