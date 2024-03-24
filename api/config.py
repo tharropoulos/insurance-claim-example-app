88bce84d8ec45e47257a63934406305cadf30942
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-03-24 12:13:19 +0200

    fix: update path of .env to read

--- api/config.py
+++ api/config.py
@@ -1,15 +1,19 @@
 import os
+from os.path import dirname as up
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, ".env"))
+env = os.path.join(up(basedir), ".env")
+load_dotenv(env)
 
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "SQLALCHEMY_DATABASE_URI"
-    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+    SQLALCHEMY_DATABASE_URI = (
+        f"sqlite+{os.environ.get('TURSO_DATABASE_URL')}/?authToken={os.environ.get('TURSO_AUTH_TOKEN')}&secure=true"
+        or "sqlite:///" + os.path.join(basedir, "app.db")
+    )
+    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_ENGINE_OPTIONS = {
         "connect_args": {"check_same_thread": False, "timeout": 30},
         "echo": True,
