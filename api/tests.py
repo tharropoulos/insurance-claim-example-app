717b96eca70af84d055cf6398b389bdc2b0c21f8
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-03-25 16:03:43 +0200

    tests: add tests for claim creation

--- api/tests.py
+++ api/tests.py
@@ -2,8 +2,10 @@ import pytest
 from flask_jwt_extended import create_refresh_token, create_access_token
 from flask import jsonify
 from config import TestConfig
-from app import create_app, db
-from app.models import User
+from app import create_app, db, supabase
+from app.models import User, Claim
+from werkzeug.datastructures import FileStorage
+import io
 
 
 @pytest.fixture
@@ -13,12 +15,16 @@ def client():
     with app.test_client() as client:
         with app.app_context():
             db.create_all()
-            yield client
+            yield client, app
             db.session.remove()
             db.drop_all()
+            supabase.client.storage.empty_bucket(
+                app.config.get("SUPABASE_STORAGE_BUCKET")
+            )
 
 
 def test_register_new_user(client):
+    client, app = client
     # Act
     response = client.post(
         "/api/auth/register",
@@ -34,6 +40,7 @@ def test_register_new_user(client):
 
 
 def test_register_duplicate_user(client):
+    client, app = client
     # Arrange
     client.post(
         "/api/auth/register",
@@ -52,6 +59,7 @@ def test_register_duplicate_user(client):
 
 
 def test_login_existing_user(client):
+    client, app = client
     # Arrange
     user = User(email="test@example.com")
     user.set_password("testpassword")
@@ -71,6 +79,7 @@ def test_login_existing_user(client):
 
 
 def test_login_non_existing_user(client):
+    client, app = client
     # Act
     response = client.post(
         "/api/auth/login",
@@ -84,6 +93,7 @@ def test_login_non_existing_user(client):
 
 # TODO: make this truly a unit test, by mocking the load_user function
 def test_whoami(mocker, client):
+    client, app = client
     # Arrange
     mock_user = User(email="test@example.com")
     mocker.patch("app.routes.load_user", return_value=mock_user)
@@ -103,6 +113,7 @@ def test_whoami(mocker, client):
 
 
 def test_whoami_without_token(client):
+    client, app = client
     # Act
     response = client.get("/api/auth/whoami")
 
@@ -112,6 +123,7 @@ def test_whoami_without_token(client):
 
 
 def test_whoami_with_invalid_token(mocker, client):
+    client, app = client
     # Act
     response = client.get(
         "/api/auth/whoami", headers={"Authorization": "Bearer invalid_token"}
@@ -123,6 +135,7 @@ def test_whoami_with_invalid_token(mocker, client):
 
 
 def test_refresh(mocker, client):
+    client, app = client
     # Arrange
     mock_user = User(email="test@example.com")
     mocker.patch("app.routes.load_user", return_value=mock_user)
@@ -142,6 +155,7 @@ def test_refresh(mocker, client):
 
 
 def test_refresh_with_invalid_token(client):
+    client, app = client
     # Act
     response = client.post(
         "/api/auth/refresh", headers={"Authorization": "Bearer invalid_token"}
@@ -153,9 +167,192 @@ def test_refresh_with_invalid_token(client):
 
 
 def test_refresh_without_token(client):
+    client, app = client
     # Act
     response = client.post("/api/auth/refresh")
 
     # Assert
     assert response.status_code == 401
     assert "access_token" not in response.get_json()
+
+
+def test_create_claim(mocker, client):
+    client, app = client
+    # Arrange
+    mock_user = User(email="test@example.com")
+    db.session.add(mock_user)
+    db.session.commit()
+    mocker.patch("app.routes.load_user", return_value=mock_user)
+
+    image_name = "none-image.jpg"
+    image_contents = b"some data"
+    mocker.patch("flask_jwt_extended.get_jwt_identity", return_value=mock_user.email)
+    access_token = create_access_token(identity=mock_user.email)
+    data = {
+        "policy_number": "who",
+        "date_of_accident": "2024-03-24T22:00:00.000Z",
+        "accident_type": "an",
+        "description": "i",
+        "damage_details": "lol",
+        "injuries_reported": True,
+        "images[0]": FileStorage(
+            stream=io.BytesIO(image_contents),
+            filename=image_name,
+            content_type="image/jpeg",
+        ),
+    }
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    # Act
+    response = client.post(
+        "/api/claims", data=data, headers=headers, content_type="multipart/form-data"
+    )
+    # Assert
+    assert response.status_code == 201  # check status code
+    json_data = response.get_json()
+    assert json_data["message"] == "Claim created successfully"
+    assert "claim_id" in json_data
+    assert "access_token" in json_data
+
+    # Check that the claim was added to the database
+    claim = Claim.query.filter(Claim.id == "1").first()
+    assert claim is not None
+    assert claim.accident_type == data["accident_type"]
+    assert claim.description == data["description"]
+
+    image = supabase.client.storage.from_(
+        app.config.get("SUPABASE_STORAGE_BUCKET")
+    ).download(image_name)
+
+    assert image is not None
+    assert image == image_contents
+
+
+def test_create_claim_no_auth(mocker, client):
+    client, app = client
+    # Arrange
+    image_name = "none-image.jpg"
+    image_contents = b"some data"
+    data = {
+        "policy_number": "who",
+        "date_of_accident": "2024-03-24T22:00:00.000Z",
+        "accident_type": "an",
+        "description": "i",
+        "damage_details": "lol",
+        "injuries_reported": True,
+        "images[0]": FileStorage(
+            stream=io.BytesIO(image_contents),
+            filename=image_name,
+            content_type="image/jpeg",
+        ),
+    }
+    headers = {"Authorization": f"Bearer no-token"}
+
+    # Act
+    response = client.post(
+        "/api/claims", data=data, headers=headers, content_type="multipart/form-data"
+    )
+    # Assert
+    assert response.status_code == 401
+
+
+def test_create_claim_wrong_file_type(mocker, client):
+    client, app = client
+    # Arrange
+    mock_user = User(email="test@example.com")
+    db.session.add(mock_user)
+    db.session.commit()
+    mocker.patch("app.routes.load_user", return_value=mock_user)
+
+    image_name = "none-image.jpg"
+    image_contents = b"some data"
+    mocker.patch("flask_jwt_extended.get_jwt_identity", return_value=mock_user.email)
+    access_token = create_access_token(identity=mock_user.email)
+    data = {
+        "policy_number": "who",
+        "date_of_accident": "2024-03-24T22:00:00.000Z",
+        "accident_type": "an",
+        "description": "i",
+        "damage_details": "lol",
+        "injuries_reported": True,
+        "images[0]": FileStorage(
+            stream=io.BytesIO(image_contents),
+            filename=image_name,
+            content_type="video/mp4",
+        ),
+    }
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    # Act
+    response = client.post(
+        "/api/claims", data=data, headers=headers, content_type="multipart/form-data"
+    )
+
+    # Assert
+    assert response.status_code == 400
+    assert response.get_json()["error"] == "File type not allowed"
+
+
+def test_create_claim_missing_file(mocker, client):
+    client, app = client
+    # Arrange
+    mock_user = User(email="test@example.com")
+    db.session.add(mock_user)
+    db.session.commit()
+    mocker.patch("app.routes.load_user", return_value=mock_user)
+
+    mocker.patch("flask_jwt_extended.get_jwt_identity", return_value=mock_user.email)
+    access_token = create_access_token(identity=mock_user.email)
+    data = {
+        "policy_number": "who",
+        "date_of_accident": "2024-03-24T22:00:00.000Z",
+        "accident_type": "an",
+        "description": "i",
+        "damage_details": "lol",
+        "injuries_reported": True,
+    }
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    # Act
+    response = client.post(
+        "/api/claims", data=data, headers=headers, content_type="multipart/form-data"
+    )
+
+    # Assert
+    assert response.status_code == 400
+    assert response.get_json()["error"] == "No file part in the request"
+
+
+def test_create_claim_missing_property(mocker, client):
+    client, app = client
+    # Arrange
+    mock_user = User(email="test@example.com")
+    db.session.add(mock_user)
+    db.session.commit()
+    mocker.patch("app.routes.load_user", return_value=mock_user)
+
+    image_name = "none-image.jpg"
+    image_contents = b"some data"
+    mocker.patch("flask_jwt_extended.get_jwt_identity", return_value=mock_user.email)
+    access_token = create_access_token(identity=mock_user.email)
+    data = {
+        "policy_number": "who",
+        "date_of_accident": "2024-03-24T22:00:00.000Z",
+        "description": "i",
+        "damage_details": "lol",
+        "injuries_reported": True,
+        "images[0]": FileStorage(
+            stream=io.BytesIO(image_contents),
+            filename=image_name,
+            content_type="image/jpeg",
+        ),
+    }
+    headers = {"Authorization": f"Bearer {access_token}"}
+
+    # Act
+    response = client.post(
+        "/api/claims", data=data, headers=headers, content_type="multipart/form-data"
+    )
+
+    # Assert
+    assert response.status_code == 400
