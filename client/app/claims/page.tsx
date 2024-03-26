37d05b371c41b36b6e7e0771df656e88fa351a50
Fanis Tharropoulos <ftharropoulos@gmail.com>
2024-03-26 17:37:42 +0200

    feat(ui): add create button on claims page

--- client/app/claims/page.tsx
+++ client/app/claims/page.tsx
@@ -1,14 +1,25 @@
+"use client";
 import ClaimsTable from "@/components/tables/claim-table";
+import { Button } from "@/components/ui/button";
 import { Toaster } from "sonner";
+import { useRouter } from "next/navigation";
 
 export default function ClaimPage() {
+    const router = useRouter();
     return (
         <main className="relative flex min-h-screen w-full justify-start lg:container ">
             <div className="flex w-full flex-col  pt-5 lg:pt-4">
-                <h1 className="text-4xl font-bold tracking-tighter">Claims</h1>
-                <h5 className="text-lg text-slate-400 dark:text-slate-500">
-                    Here you can manage all your claims
-                </h5>
+                <div className="flex w-full justify-between">
+                    <div>
+                        <h1 className="text-4xl font-bold tracking-tighter">Claims</h1>
+                        <h5 className="text-lg text-slate-400 dark:text-slate-500">
+                            Here you can manage all your claims
+                        </h5>
+                    </div>
+                    <Button variant={"default"} onClick={() => router.push("/claims/create")}>
+                        Create Claim
+                    </Button>
+                </div>
                 <ClaimsTable />
                 <Toaster
                     position="bottom-left"
